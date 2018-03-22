80feec4a192bb6b3e361b2b8977df526adff404e
Noskcaj <kcajdev@gmail.com>
2018-03-22 01:07:47 -0400

    Add u16 and u32 port functions
    Add volatile to ports

--- src/drivers/ports.rs
+++ src/drivers/ports.rs
@@ -1,13 +1,43 @@
-/// Read data from `port`
-pub fn byte_in(port: usize) -> u8 {
+#![allow(dead_code)]
+
+/// Read a `u8` from `port`
+pub fn inb(port: u16) -> u8 {
     let result: u8;
-    unsafe { asm!("in %dx, %al" : "={al}"(result) : "{dx}"(port)) }
+    unsafe { asm!("inb %dx, %al" : "={al}"(result) : "{dx}"(port) :: "volatile") }
     result
 }
 
-/// Write `data` to `port`
-pub fn byte_out(port: usize, data: u8) {
+/// Write a `u8` to `port`
+pub fn outb(port: u16, data: u8) {
     unsafe {
-        asm!("out %al, %dx" :: "{al}"(data), "{dx}"(port));
+        asm!("outb %al, %dx" :: "{al}"(data), "{dx}"(port) :: "volatile");
+    }
+}
+
+/// Read a `u16` from `port`
+pub fn inw(port: u16) -> u16 {
+    let result: u16;
+    unsafe { asm!("inw %dx, %ax" : "={ax}"(result) : "{dx}"(port) :: "volatile") }
+    result
+}
+
+/// Write a `u16` to `port`
+pub fn outw(port: u16, data: u16) {
+    unsafe {
+        asm!("outw %ax, %dx" :: "{ax}"(data), "{dx}"(port) :: "volatile");
+    }
+}
+
+/// Read a `u32` from `port`
+pub fn inl(port: u16) -> u32 {
+    let result: u32;
+    unsafe { asm!("inl %dx, %eax" : "={eax}"(result) : "{dx}"(port) :: "volatile") }
+    result
+}
+
+/// Write a `u32` to `port`
+pub fn outl(port: u16, data: u32) {
+    unsafe {
+        asm!("outl %eax, %dx" :: "{eax}"(data), "{dx}"(port) :: "volatile");
     }
 }
